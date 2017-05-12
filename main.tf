1b529edabbb1b299579b7a41bb42da86fda8ca6e
Rohit Yadav <rohit.yadav@manheim.com>
2017-05-12 14:45:30 -0400

    Upgraded to use terraform versio 0.9 backend.

--- main.tf
+++ main.tf
@@ -14,6 +14,19 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-output "bucket_name" {
+terraform {
+  backend "s3" {
+    bucket = "rohit-terraform-state-storage"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
+
+output "bucket_arn" {
   value = "${aws_s3_bucket.terraform_state.arn}"
+}
+
+
+output "bucket_name" {
+  value = "${aws_s3_bucket.terraform_state.bucket}"
 }
